4e13007c28b9c0f8e55c20f88fecfa087008db6b
hwanyb <mnn9502@naver.com>
2022-02-26 11:00:00 +0900

    Add Delete To Do

--- vanilla-todo/src/index.js
+++ vanilla-todo/src/index.js
@@ -7,12 +7,26 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+const addToDo = text => {
+  return {
+    type: ADD_TODO,
+    text,
+  };
+};
+
+const deleteToDo = id => {
+  return {
+    type: DELETE_TODO,
+    id
+  };
+};
+
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { text: action.text, id: Date.now() }];
+      return [{ text: action.text, id: Date.now() }, ...state];
     case DELETE_TODO:
-      return [];
+      return state.filter(toDo => toDo.id !== action.id);
     default:
       return state;
   }
@@ -20,31 +34,38 @@ const reducer = (state = [], action) => {
 
 const store = createStore(reducer);
 
+store.subscribe(() => console.log(store.getState()));
+
+const dispatchAddToDo = text => {
+  store.dispatch(addToDo(text));
+};
+
+const dispatchDelteToDo = e => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
+};
+
 const paintToDos = () => {
   const toDos = store.getState();
-  toDos.forEach((toDo) => {
+  ul.innerHTML = "";
+  toDos.forEach(toDo => {
     const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.className = "delBtn";
+    delBtn.innerText = "🗑";
+    delBtn.addEventListener("click", dispatchDelteToDo);
     li.id = toDo.id;
     li.innerText = toDo.text;
     ul.appendChild(li);
+    li.appendChild(delBtn);
   });
 };
 
 store.subscribe(paintToDos);
-store.subscribe(() => console.log(store.getState()));
 
-// const createToDo = (toDo) => {
-//   const li = document.createElement("li");
 
-//   li.innerText = toDo;
-//   ul.appendChild(li);
-// };
 
-const addToDo = (text) => {
-  store.dispatch({ type: ADD_TODO, text: text });
-}
-
-const onSubmit = (e) => {
+const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
@@ -52,7 +73,7 @@ const onSubmit = (e) => {
   if (toDo === "") {
     alert("You need to write To do!");
   } else {
-    addToDo(toDo);
+    dispatchAddToDo(toDo);
   }
 };
 
