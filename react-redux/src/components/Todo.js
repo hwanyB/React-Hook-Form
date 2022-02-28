a2c814e66ff118a598b2e4b7af6f24cebf09f076
hwanyb <mnn9502@naver.com>
2022-02-28 11:00:00 +0900

    Detail Page

--- react-redux/src/components/Todo.js
+++ react-redux/src/components/Todo.js
@@ -1,20 +1,25 @@
-import React from 'react'
-import { connect } from 'react-redux';
-import '../routes/Home.css'
-import { actionCreators } from '../store';
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import "../routes/Home.css";
+import { actionCreators } from "../store";
 
-export function Todo({text, onDelBtnClick}) {
-    
+export function Todo({ text, onDelBtnClick, id }) {
   return (
-    <li>{text}
-        <button onClick={onDelBtnClick} className='delBtn'>🗑</button>
+    <li>
+      <Link className='Link' to={`/${id}`}>
+        <p>{text}</p>
+      </Link>
+      <button onClick={onDelBtnClick} className='delBtn'>
+        🗑
+      </button>
     </li>
   );
 }
-function mapDispatchToProps(dispatch,ownProps){
-    return {
-        onDelBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id))
-    };
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+    onDelBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id)),
+  };
 }
 
 export default connect(null, mapDispatchToProps)(Todo);
--- react-redux/src/routes/Detail.js
+++ react-redux/src/routes/Detail.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { connect } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
-export default function Detail() {
+export function Detail({ toDo }) {
+  const myId = useParams().id;
+  const findToDo = toDo.find(toDo => toDo.id === parseInt(myId));
+
+  const date = new Date(findToDo?.id).toString();
+  console.log(date);
   return (
-    <div>Detail</div>
+    <div>
+      <h1>{findToDo?.text}</h1>
+      <h5>Created At : {date}</h5>
+    </div>
   )
 }
+
+function mapStateToProps(state, ownProps) {
+  return { toDo: state }
+}
+export default connect(mapStateToProps)(Detail);
